9052a68efc6b3bd0d25fdec29e0aeccfdf1946e9
mohammad <69156294+mohammad-ashrafi2020@users.noreply.github.com>
2025-01-22 11:12:29 +0330

    Delete and Change Comment Status

--- pages/comments/index.vue
+++ pages/comments/index.vue
@@ -14,11 +14,21 @@
             ]">{{ GetUserRecommendedStatusName(slotProps.data.userRecommendedStatus) }}</p>
           </template>
         </Column>
+        <Column field="status" header="وضعیت">
+          <template #body="slotProps">
+            <span class="text-green-600 text-sm mr-1" v-if="slotProps.data?.status == CommentStatus.Accepted">تایید
+              شده</span>
+            <span class="text-orange-600 text-sm mr-1" v-else-if="slotProps.data?.status == CommentStatus.Pending">در
+              انتظار
+              تایید</span>
+            <span class="text-red-600 text-sm mr-1" v-else>رد شده</span>
+          </template>
+        </Column>
         <Column field="id" header="عملیات">
           <template #body="slotProps">
             <div class="flex gap-2">
               <Button @click="openDialog(slotProps.data)" size="small" severity="info" label="نمایش"></Button>
-              <Button size="small" severity="danger">حذف</Button>
+              <Button size="small" @click="openDeleteDialog(slotProps.data)" severity="danger">حذف</Button>
             </div>
           </template>
         </Column>
@@ -59,24 +69,41 @@
         </div>
         <hr class="my-5" />
         <p class="w-full"><b>متن نظر</b> : {{ selectedComment?.text }}</p>
+        <hr class="my-5" />
+        <p>تغییر وضعیت نظر : </p>
+        <BaseSelectBox name="chagneStatus" v-model="selectedStatus" :options="[{
+          name: 'در انتظار تایید',
+          value: CommentStatus.Pending.toString()
+        },
+        {
+          name: 'رد شده',
+          value: CommentStatus.Rejected.toString()
+        },
+        {
+          name: 'تایید شده',
+          value: CommentStatus.Accepted.toString()
+        }]"></BaseSelectBox>
+        <div class="flex justify-end mt-4">
+          <Button @click="changeStatus">ثبت تغییرات</Button>
+        </div>
       </div>
     </Dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { usePrimeFunctions } from '~/composables/usePrimeFunctions';
 import { CommentStatus, type Comment, type CommentFilterParams } from '~/models/comments/Comment';
 import type { FilterResult } from '~/models/Filterresult';
-import { getComments } from '~/services/comment.service';
+import { deleteComment, getComments, changeCommentStatus } from '~/services/comment.service';
 import { GetUserRecommendedStatusName } from '~/utils/EnumConvertor';
 
 const comments: Ref<FilterResult<Comment> | null> = ref(null);
 const selectedComment: Ref<Comment | null> = ref(null);
-
+const primeFunctions = usePrimeFunctions();
 const loading = ref(false);
-
 const isOpenDialog = ref(false);
-
+const selectedStatus = ref(CommentStatus.Accepted.toString())
 
 const filterParams = reactive<CommentFilterParams>({
   commentStatus: undefined,
@@ -92,7 +119,18 @@ onMounted(async () => {
 definePageMeta({
   title: "مدیریت نظرات",
 });
-
+const openDeleteDialog = (comment: Comment) => {
+  selectedComment.value = comment;
+  primeFunctions.deleteDialog(undefined, undefined, DeleteComment);
+}
+const DeleteComment = async () => {
+  if (selectedComment.value) {
+    var res = await deleteComment(selectedComment.value.id);
+    if (res.isSuccess) {
+      getData();
+    }
+  }
+}
 const getData = async () => {
   loading.value = true;
   var result = await getComments(filterParams);
@@ -110,7 +148,19 @@ const changeTake = (newTake: number) => {
 }
 const openDialog = (comment: any) => {
   selectedComment.value = comment;
+  selectedStatus.value = comment.status.toString();
   isOpenDialog.value = true;
+}
+const changeStatus = async () => {
+  if (selectedComment.value) {
+    selectedComment.value.status = parseInt(selectedStatus.value);
+    var res = await changeCommentStatus(selectedComment.value.id, Number(selectedStatus.value));
+    if (res.isSuccess) {
+      primeFunctions.successToast();
+      getData();
+    }
+    // isOpenDialog.value = false;
+  }
 }
 </script>
 
--- services/comment.service.ts
+++ services/comment.service.ts
@@ -1,9 +1,24 @@
-import type { Comment, CommentFilterParams } from "~/models/comments/Comment";
+import type {
+  Comment,
+  CommentFilterParams,
+  CommentStatus,
+} from "~/models/comments/Comment";
 import type { FilterResult } from "~/models/Filterresult";
 import { CustomFetch } from "~/utils/CustomFetch";
 
 export const getComments = (filterParams: CommentFilterParams) => {
   return CustomFetch<FilterResult<Comment>>("/comment", {
     query: filterParams,
+  });
+};
+export const deleteComment = (id: number) => {
+  return CustomFetch(`/comment/${id}`, {
+    method: "DELETE",
+  });
+};
+export const changeCommentStatus = (id: number, status: CommentStatus | number) => {
+  return CustomFetch(`/comment/changeStatus`, {
+    method: "PUT",
+    body: { status, id },
   });
 };
